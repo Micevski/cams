789d5e79c0cf754eb1281b75ccfc2192e2845d7f
David Micevski <david.micevski@sorsix.com>
2020-03-31 20:51:49 +0200

    Add user from admin

--- ng-cams/src/app/app-routing.module.ts
+++ ng-cams/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {HomeComponent} from "./pages/home/home.component";
 import {AccidentComponent} from "./pages/accident/accident.component";
 import {AuthenticationGuard} from "./guards/authentication-guard";
 import {LoginComponent} from "./pages/login/login.component";
+import { AdminComponent } from './pages/admin/admin.component';
 
 
 const routes: Routes = [
   {
     path: '', canActivateChild: [AuthenticationGuard], children: [
       {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'admin', component: AdminComponent},
       {path: 'home', component: HomeComponent},
       {path: 'accident/create', component: AccidentComponent}
     ]
